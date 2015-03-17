4955facb0df2a2fe1c785d9734314a30d199bdb8
Mick Bass <mick.bass@47lining.com>
2015-03-16 23:35:34 -0600

    documentation assembly process fixes

--- documentation/assemble.sh
+++ documentation/assemble.sh
@@ -10,13 +10,10 @@ cp --archive $NUCLEATOR_CORE/documentation/site/* $TARGET/
 cp --archive $CONFLUENCE_EXPORT/* $TARGET/documentation/
 cd $TARGET/documentation
 find -name "Nucleator-Documentation_*.html" -exec echo making '{}' a symlink to index.html \; -exec rm '{}' \; -exec ln -s index.html '{}' \;
-Nucleator-Documentation_22052969.html
 cd $NUCLEATOR_CORE/documentation/reference
 make clean
 make all
 cp *.html $TARGET/documentation
-sed --in-place='' 's/href="Nucleator-CLI-Reference.*\.html"/href="nucleator.html"/' $TARGET/documentation/index.html
-sed --in-place='' 's/href="Quick-Start-Guide.*\.html"/href="nucleatorquickstart.html"/' $TARGET/documentation/index.html
+sed --in-place='' 's/href="Nucleator-CLI-Reference_22053054.html"/href="nucleator.html"/g' $TARGET/documentation/index.html
+sed --in-place='' 's/href="Quick-Start-Guide_22052991.html"/href="nucleatorquickstart.html"/g' $TARGET/documentation/index.html
 make clean
-
-
