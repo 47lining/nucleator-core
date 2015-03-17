7ecbb604627f761563ed68192d583643920205a9
Brian Lloyd <brian.d.lloyd@gmail.com>
2015-03-17 13:16:29 -0400

    add tojsonfragment filter

---
+++ lib/nucleator/core/foundation/ansible/filter_plugins/tojsonfragment.py
@@ -0,0 +1,30 @@
+# Copyright 2015 47Lining LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+def to_json_fragment(Entries, stripdepth=1):
+        """Return a json fragment representing the provided objects"""
+
+        return json.dumps(Entries, indent=4)[stripdepth:][:-stripdepth]
+
+class FilterModule(object):
+    ''' Custom filters are loaded by FilterModule objects '''
+
+    def filters(self):
+        ''' FilterModule objects return a dict mapping filter names to
+            filter functions. '''
+        return {
+            'to_json_fragment': to_json_fragment
+        }
